69c39ec0edaf5f42932ae297f785346e1d177b9f
Suhaib King <suhaibking310@gmail.com>
2024-07-25 14:10:25 +0530

    rmove

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 
 app.use(cors());
 
+const publicPath = path.join(__dirname, "api");
+
+app.use(express.static(publicPath));
+
 app.get("/", (req, res) => {
-  res.send("OK");
+    res.send("OK");
 });
 
 app.get("/api/localize/:lang", (req, res) => {
-  const lang = req.params.lang;
-  res.sendFile(path.join(__dirname, "api/localize", `${lang}.json`));
+    const lang = req.params.lang;
+    res.sendFile(path.join(publicPath, `localize/${lang}.json`));
+});
+
+app.get("/api/localize/list", (req, res) => {
+    res.json({
+        "en": "English"
+    });
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`The server is running on the port ${port}`);
 });
